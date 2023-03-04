095aee32f56d470e3157a35d9cd41b3ecbcf0e8c
lixin59 <1453287107@qq.com>
2023-03-04 10:33:22 +0800

    chore(projects): 移除不需要使用的路由和模块

--- src/router/modules/function.ts
+++ src/router/modules/function.ts
@@ -13,31 +13,6 @@ const functionRoute: AuthRoute.Route = {
         icon: 'ic:round-tab'
       }
     }
-    // {
-    //   name: 'function_tab-detail',
-    //   path: '/function/tab-detail',
-    //   component: 'self',
-    //   meta: {
-    //     title: 'Tab Detail',
-    //     requiresAuth: true,
-    //     hide: true,
-    //     activeMenu: 'function_tab',
-    //     icon: 'ic:round-tab'
-    //   }
-    // },
-    // {
-    //   name: 'function_tab-multi-detail',
-    //   path: '/function/tab-multi-detail',
-    //   component: 'self',
-    //   meta: {
-    //     title: 'Tab Multi Detail',
-    //     requiresAuth: true,
-    //     hide: true,
-    //     multiTab: true,
-    //     activeMenu: 'function_tab',
-    //     icon: 'ic:round-tab'
-    //   }
-    // }
   ],
   meta: {
     title: '功能',
